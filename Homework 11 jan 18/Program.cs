308621f40667c839e670f1457ea055f1d7c2dd17
Elias Boiko <elias.boiko@gmail.com>
2018-01-11 00:50:48 +0200

    1

--- Homework 11 jan 18/Program.cs
+++ Homework 11 jan 18/Program.cs
@@ -10,6 +10,46 @@ namespace Homework_11_jan_18
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
+            byte?[] miniarray = new byte?[20];
+            int a = 0;
+
+            for (int i = 0; i < miniarray.Length; i++)
+            {
+                while (miniarray[i] == null)
+                {
+                    try
+                    {
+                        a = rand.Next(-5, 28);
+                    }
+                    catch (IndexOutOfRangeException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    try
+                    {
+                        while (miniarray[a] == null)
+                            miniarray[a] = (byte)rand.Next(0, 1000); // без (byte) оно не хочет рандомить
+                    }
+                    catch (OutOfMemoryException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    catch (IndexOutOfRangeException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+            }
+            foreach (var item in miniarray)
+            {
+                Console.WriteLine(item);
+            }
+
         }
+
+
     }
 }
+
